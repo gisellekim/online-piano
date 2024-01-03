74085faa046604812c42e84775a738eef35cbc07
giselle <giselle.kim@eyos.one>
2024-01-03 22:55:53 +0000

    chore: connect Main component to the front page

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,14 +3,8 @@ import styles from "./Logo.module.css"
 export const Logo = () => {
   return (
     <h1 className={styles.logo}>
-      <span role="img" aria-label="metal hand emoji">
-        * Metal Hand Emoji *
-      </span>
       <span role="img" aria-label="musical keyboard emoji">
-        * Musical Keyboard Emoji *
-      </span>
-      <span role="img" aria-label="musical notes emoji">
-        * Musical Notes Emoji *
+        🎹
       </span>
     </h1>
   )
