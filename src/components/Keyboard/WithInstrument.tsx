6e4efa39cc8646af3411a83801d1e4f9daa7ed3d
giselle <giselle.kim@eyos.one>
2024-01-04 17:43:21 +0000

    feat: updated KeyboardWithInstrument to load a selected instrument

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,12 +1,17 @@
+import { useEffect } from "react"
 import { Keyboard } from "../Keyboard"
 import { useAudioContext } from "../AudioContext"
 import { useSoundfont } from "../../adapters/Soundfont"
-import { useMount } from "../../utils/useMount"
+import { useInstrument } from "../../state/Instrument"
 
 export const KeyboardWithInstrument = () => {
   const AudioContext = useAudioContext()!
-  const { loading, load, play, stop } = useSoundfont({ AudioContext })
+  const { instrument } = useInstrument()
+  const { loading, load, play, stop, current } = useSoundfont({ AudioContext })
 
-  useMount(() => load())
+  useEffect(() => {
+    if (!loading && instrument !== current) load(instrument)
+  }, [load, loading, current, instrument])
+
   return <Keyboard loading={loading} play={play} stop={stop} />
 }
